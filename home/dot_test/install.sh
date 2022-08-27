c8f34abb255aa68f08105fd45e8c125ee0687fcf
garryshield <749291704@qq.com>
2022-08-27 23:33:18 +0800

    init

--- home/dot_test/install.sh
+++ home/dot_test/install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y apt-utils sudo git curl wget
+apt-get install -y curl
+
+# chezmoi
+sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$HOME/.local/bin"
+chezmoi="$HOME/.local/bin/chezmoi"
+exec "$chezmoi" init garryshield
+
+
+
+
